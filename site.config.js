17663031f2a01bd45f8841b0f16235ee42ca3340
parkminwoo <parkminwoo1991@gmail.com>
2023-04-11 20:36:31 +0900

    chore: change profile img

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "parkmiwnoo",
-    image: "/thinking.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/the-thinker-3d.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Developer",
     bio: "Record and share research findings.",
     email: "parkmiwnoo1991@gmail.com",
